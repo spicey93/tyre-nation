1e6c0aaae70976e8fc85f46dd2360feac00b92af
Dean Spicer <spicerdean@icloud.com>
2023-01-02 15:26:24 +0000

    robots-txt added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
+import robotsTxt from 'astro-robots-txt'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://tyrenation.co.uk',
+    integrations: [robotsTxt()],
+});
